77d03377eea4f1f33a3fe5c9ff891484d8c2abce
misael <misael.vergara@softcenter.com.br>
2020-05-07 17:37:17 -0300

    15.1

--- example/src/App.js
+++ example/src/App.js
@@ -6,11 +6,14 @@ const App = () => {
   return (
     <>
       <Input
-        // error={true}
+        error={true}
         errorMsg='lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum  '
-
       />
-      <Button onClick={() => console.log('123123')} disabled={false} text='Botão' />
+      <Button
+        onClick={() => console.log('123123')}
+        disabled={false}
+        text='Botão'
+      />
     </>
   )
 }
