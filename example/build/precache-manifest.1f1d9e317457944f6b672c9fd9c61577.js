ebd979f1da9113fff63f3d58adf5fff609debda3
misael <misael.vergara@softcenter.com.br>
2020-05-12 14:16:09 -0300

    fix: missing readme on npm page

---
+++ example/build/precache-manifest.1f1d9e317457944f6b672c9fd9c61577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0b8dfd0ee538937a2736818dbcd3cd",
+    "url": "/react-basic-components/index.html"
+  },
+  {
+    "revision": "c1a9822b6f73b037d622",
+    "url": "/react-basic-components/static/css/main.0aba03a9.chunk.css"
+  },
+  {
+    "revision": "159042629b6a3d001cce",
+    "url": "/react-basic-components/static/js/2.ae4b9016.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-basic-components/static/js/2.ae4b9016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a9822b6f73b037d622",
+    "url": "/react-basic-components/static/js/main.1f80b932.chunk.js"
+  },
+  {
+    "revision": "090ba1d9c403e631714a",
+    "url": "/react-basic-components/static/js/runtime-main.b0028cfc.js"
+  }
+]);
--- example/src/App.js
+++ example/src/App.js
@@ -1,20 +1,44 @@
 import React from 'react'
 
 import { Button, Input } from 'react-basic-components'
+import styled from 'styled-components'
+
+const MyDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 60px 1fr 100px;
+
+  .red {
+    background: red;
+    width: 100%;
+    height: 100%;
+  }
+
+  .black {
+    background: grey;
+    width: 100%;
+    height: 100%;
+    font-size: 65px;
+  }
+
+  .blue {
+    background: blue;
+    width: 100%;
+    height: 100%;
+    font-size: 1rem;
+  }
+`
 
 const App = () => {
   return (
     <>
-      <Input
-        error={true}
-        errorMsg='lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum  '
-        type='password'
-      />
-      <Button
-        onClick={() => console.log('123123')}
-        disabled={false}
-        text='Botão'
-      />
+      <MyDiv>
+        <div className='red'></div>
+        <div className='black'>preto</div>
+        <div className='blue'>azul</div>
+      </MyDiv>
     </>
   )
 }
