844612875b4b953649ce0c8738b795a49c1655da
Gustavo Braguini <gustavo.braguini@softcenter.com.br>
2020-05-27 15:04:03 -0300

    Corrigido a passagem do className

--- src/LabeledInput/index.js
+++ src/LabeledInput/index.js
@@ -3,9 +3,9 @@ import { StyledLabeledInput } from './style'
 import { Input } from '../Input'
 
 export const LabeledInput = React.forwardRef(
-  ({ title, fontSize = '18px', ...props }, ref) => {
+  ({ title, fontSize = '18px', className, ...props }, ref) => {
     return (
-      <StyledLabeledInput fontSize={fontSize}>
+      <StyledLabeledInput className={className} fontSize={fontSize}>
         <span className='field-title'>{title}</span>
 
         <Input {...props} ref={ref} />
